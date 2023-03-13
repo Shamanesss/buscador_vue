82deed52061f3d8c4f0f67c3f9b6abacb215cbdd
Shamanesss <mentulbenodorna@gmail.com>
2023-03-13 22:23:37 +0100

    añadiendo estilos

--- buscador_vue/src/components/HelloWorld.vue
+++ buscador_vue/src/components/HelloWorld.vue
@@ -1,35 +1,19 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="buscador">
+   <article class="content">
+            <h1 class="content__title">Search GitHub users</h1>
+
+            <!-- Search -->
+            <form class="search">
+                <input type="text" class="search__input" required placeholder="Search GitHub users">
+                <input type="submit" class="search__submit" value="Search">
+            </form>
+
+            <!-- Result -->
+            <!-- Error -->
+            <div class="result__error"></div>
+        </article>
+      </div>
 </template>
 
 <script>
@@ -43,18 +27,46 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.buscador {
+  font-family: sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(227, 226, 230);
+  font-size: 1.3rem;
+  padding: 17rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+/* Content */
+.content {
+  background-color: #42b983;
+  width: 768px;
+  max-width: 768px;
+  padding: 3rem;
+  box-shadow: 2px 2px 3px gray;
+  border-radius: 1rem;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.content__title {
+  margin: 1rem;
+  text-align: center;
 }
-a {
-  color: #42b983;
+.search {
+  display: flex;
+  margin-bottom: 2.5rem;
 }
+
+.search__input {
+  flex: 1;
+  padding: 1rem;
+  font-size: 1rem;
+}
+.result__error {
+  padding: 0.3rem;
+  background-color: tomato;
+  color: white;
+  text-align: center;
+  border: 1px solid red;
+}
+
 </style>
